1b5c589eaed087511d2542a2533ceec82df1d6ec
Anmol Sharma <anmolsharma6503@gmail.com>
2025-04-05 15:25:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ import path from "path";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://moonlit-threads-frontend.vercel.app", credentials: true }));
 app.use(cookieParser()); // allows us to parse incoming cookies
   
 
